939332320a14a072103f1c74c899bf7d0084142c
jrejoire <jrejoire@hotmail.fr>
2020-03-01 19:55:12 +0100

    Updates

---
+++ precache-manifest.96c321b4f4f3cb34e0be2956af90263c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b354b8d047a45329623da3078f4073c4",
+    "url": "/login_card_component/index.html"
+  },
+  {
+    "revision": "214f0c8a741c83422198",
+    "url": "/login_card_component/static/css/main.10a2b212.chunk.css"
+  },
+  {
+    "revision": "66510f71f0c72fd9c983",
+    "url": "/login_card_component/static/js/2.724bddff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/login_card_component/static/js/2.724bddff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214f0c8a741c83422198",
+    "url": "/login_card_component/static/js/main.eea9d229.chunk.js"
+  },
+  {
+    "revision": "8c3d00ae2c6650610022",
+    "url": "/login_card_component/static/js/runtime-main.6f7971ef.js"
+  },
+  {
+    "revision": "b304d6bfbe0c8010e8124113a123e89e",
+    "url": "/login_card_component/static/media/logo.b304d6bf.svg"
+  }
+]);
